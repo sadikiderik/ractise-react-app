ea98098dd93c34aa61caec0f338efebf46c3083b
derik.sadiki <derik.sadiki@sovtech.com>
2023-07-21 20:58:25 +0200

    yml 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and saveto reload eeeee.
+          Edit <code>src/App.js</code> it's time to code.
         </p>
         <a
           className="App-link"
