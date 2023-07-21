962d20b6df997a7a6a68116cf6deb936ade1e663
derik.sadiki <derik.sadiki@sovtech.com>
2023-07-21 21:54:09 +0200

    add yml channel

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> it's time to code.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
