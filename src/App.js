fcb27d3097f5a139649a8ab782bd793b8e9004a9
derik.sadiki <derik.sadiki@sovtech.com>
2023-07-21 20:47:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and saveto reload.
+          Edit <code>src/App.js</code> and saveto reload eeeee.
         </p>
         <a
           className="App-link"
